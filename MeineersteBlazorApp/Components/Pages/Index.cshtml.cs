3a34edd90434c35c55a5ada89ba06f5e303f788c
origin <https://github.com/florianforrrer/MeineerstesBlazor.git>
2025-11-22 18:19:18 +0100

    Erster sauberer Commit

---
+++ MeineersteBlazorApp/Components/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeineersteBlazorApp.Components.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MeineersteBlazorApp/obj/Debug/net10.0/MeineersteBlazorApp.AssemblyInfo.cs
+++ MeineersteBlazorApp/obj/Debug/net10.0/MeineersteBlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeineersteBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e65c36e00ff4c0b57c78f328f9c0cde76e6536")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeineersteBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeineersteBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
